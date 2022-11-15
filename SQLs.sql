fb4681b6a2208559c1a1637b29d0855eface9cc7
Pedro Somenci <pedro.somenci@gmail.com>
2022-11-14 21:43:23 -0300

    fix sql e validador

--- SQLs.sql
+++ SQLs.sql
@@ -5,14 +5,46 @@ CREATE DATABASE yalo
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
 
-CREATE TABLE estudantes (
-    id SERIAL PRIMARY KEY,
-    cpf VARCHAR(11) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
-	email VARCHAR(255) NOT NULL,
-	id_colegio INTEGER NOT NULL,
-	id_turma INTEGER NOT NULL,
-	score NUMERIC(3,1)
+CREATE TABLE colegios
+(
+    id serial NOT NULL,
+    nome character varying(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE turmas
+(
+    id serial NOT NULL,
+    nome character varying NOT NULL,
+    fk_colegio integer NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_colegio FOREIGN KEY (fk_colegio)
+        REFERENCES colegios (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
+
+CREATE TABLE estudantes
+(
+    id serial NOT NULL,
+    cpf character varying(255) NOT NULL,
+    name character varying(255) NOT NULL,
+    email character varying(255) NOT NULL,
+    id_colegio integer NOT NULL,
+    id_turma integer NOT NULL,
+    score numeric(3, 1),
+    PRIMARY KEY (id),
+    CONSTRAINT id_colegio FOREIGN KEY (id_colegio)
+        REFERENCES colegios (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT id_turma FOREIGN KEY (id_turma)
+        REFERENCES turmas (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
 );
 
 INSERT INTO estudantes (cpf, name, email, id_colegio, id_turma, score) 
--- src/validation/postEstudante.js
+++ src/validation/postEstudante.js
@@ -4,9 +4,13 @@ const { body, validationResult } = require('express-validator');
 // Valida as informações vindas do body para cadastrar o estudante
 exports.postEstudante = [
   body('cpf', 'cpf é obrigatorio').exists(),
+  body('name', 'name é obrigatorio').exists(),
+  body('email', 'email é obrigatorio').exists(),
   body('id_colegio', 'id_colegio é obrigatorio').exists(),
   body('id_turma', 'id_turma é obrigatorio').exists(),
   body('cpf', 'cpf não pode ser vazio').notEmpty(),
+  body('name', 'name não pode ser vazio').notEmpty(),
+  body('email', 'email não pode ser vazio').notEmpty(),
   body('id_colegio', 'id_colegio não pode ser vazio').notEmpty(),
   body('id_turma', 'id_turma  não pode ser vazio').notEmpty(),
 ];
